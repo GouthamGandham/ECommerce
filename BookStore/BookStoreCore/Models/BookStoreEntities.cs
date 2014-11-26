12af5f4ac6091c144b78e7091a648aebc47c5066
GGandham <gvgandham@gmail.com>
2014-11-26 14:33:09 -0600

    Uploading existing project

---
+++ BookStore/BookStoreCore/Models/BookStoreEntities.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public class BookStoreEntities : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ BookStore/BookStoreCore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using BookStore.Models;
+
+namespace BookStore.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
